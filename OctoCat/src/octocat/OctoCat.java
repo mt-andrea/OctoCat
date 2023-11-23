c8ec7d39f0752694e8526d122278f54d56ff699d
Máté Andrea <74617712+mt-andrea@users.noreply.github.com>
2023-11-23 22:43:43 +0100

    GUI setup

--- OctoCat/src/octocat/OctoCat.java
+++ OctoCat/src/octocat/OctoCat.java
@@ -21,7 +21,8 @@ public class OctoCat extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-
+        stage.setResizable(false);
+        stage.setTitle("Jelszogenerátor");
         stage.setScene(scene);
         stage.show();
     }
