966668719b394e466cd9209cd5a590c9f49ac8f4
Máté Andrea <74617712+mt-andrea@users.noreply.github.com>
2023-11-23 22:13:13 +0100

    projekt inicializáció

---
+++ OctoCat/src/octocat/OctoCat.java
@@ -0,0 +1,36 @@
+/*Made by Andrea Mate
+* For practice.
+This is the way! */
+
+package octocat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author $Mate Andrea
+ */
+public class OctoCat extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
