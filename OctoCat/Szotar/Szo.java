1dd829d4987681da9a9f3651a52628692364219d
aoekatalin <150516987+aoekatalin@users.noreply.github.com>
2023-11-29 19:47:21 +0100

    Getter, setter metódusok beállítása

--- OctoCat/Szotar/Szo.java
+++ OctoCat/Szotar/Szo.java
@@ -10,9 +10,34 @@
 public class Szo {
     private int szoID;
     private String lecke;
-    private String idegenNyel;
+    private String idegenNyelv;
     private String idegenSzo;
     private String magyar;
+    
+    public Szo(int szoID, String lecke, String idegenNyelv, String idegenSzo, String magyar){
+          this.szoID = szoID;
+          this.lecke =lecke;
+          this.idegenNyelv = idegenNyelv;
+          this.idegenSzo = idegenSzo;
+          this.magyar = magyar;
+    }
+    public int getSzoID(){
+        return szoID;
+    }
+    public String getLecke(){
+        return lecke;
+    }
+    public String getIdegenNyelv(){
+        return idegenNyelv;
+    }
+    public String getIdegenSzo(){
+        return idegenSzo;
+    }
+    public String getMagyar(){
+        return magyar;
+    }
+    
+          
     
     
 }
