ecf7b49a313a8c9a8134bbe4ac19560ecd5a8d1f
Kjanos141 <53945017+Kjanos141@users.noreply.github.com>
2023-11-30 17:56:36 +0100

    Create DB.java
    DB osztály létrehozása
    Panel osztály importálása

---
+++ OctoCat/src/octocat/DB.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Kalmár János
+ */
+package octocat;
+
+import panel.Panel;
+
+
+
+/**
+ *
+ * @author Kalmár János
+ */
+public class DB {
+    
+    final String url = "";
+    final String felhasznalo = "";
+    final String jelszo = "";
+    
+    
+}
