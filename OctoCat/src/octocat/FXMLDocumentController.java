a3d285e5ebb8ff97f2c51875b7de296de6282a0d
bezdan.anita@gmail.com <bezdan.anita@gmail.com>
2023-11-30 22:04:28 +0100

    A TableView osztály importjának áthelyezése a Controllerbe

--- OctoCat/src/octocat/FXMLDocumentController.java
+++ OctoCat/src/octocat/FXMLDocumentController.java
@@ -21,6 +21,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  *
