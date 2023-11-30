2111f0ddeee447b5731fbc4b0b20c2edb73dfcc3
András Spengler <andrisspengler@gmail.com>
2023-11-30 09:02:43 +0100

    ResourceBundle importálása
    ResourceBundle importálása a Controller osztályba

--- OctoCat/src/octocat/OctoCat.java
+++ OctoCat/src/octocat/OctoCat.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Random;
+import java.ResourceBundle;
 /**
  *
  * @author $Mate Andrea, Andorfer Katalin, András Spengler
