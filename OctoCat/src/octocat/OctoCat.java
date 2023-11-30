794dfefc7e9c64a29dcce7ade8c9958b980e3279
András Spengler <andrisspengler@gmail.com>
2023-11-30 09:00:19 +0100

    URL importálása
    URL importálása a Controller osztályba

--- OctoCat/src/octocat/OctoCat.java
+++ OctoCat/src/octocat/OctoCat.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.awt.Desktop;
 import java.net.URI;
+import java.net.URL;
 /**
  *
  * @author $Mate Andrea, Andorfer Katalin, András Spengler
