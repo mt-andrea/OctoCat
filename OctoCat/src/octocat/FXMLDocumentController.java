6cbbdb98729199563363b8eb329ae28a89bf9c96
bezdan.anita@gmail.com <bezdan.anita@gmail.com>
2023-11-30 20:24:29 +0100

    Fölösleges kódrészek kitörlése

--- OctoCat/src/octocat/FXMLDocumentController.java
+++ OctoCat/src/octocat/FXMLDocumentController.java
@@ -21,16 +21,7 @@ import javafx.scene.control.TextField;
 
 public class FXMLDocumentController implements Initializable {
 
-    @FXML
-    private TextField txtJelszo;    
-    @FXML
-    private Label label;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
